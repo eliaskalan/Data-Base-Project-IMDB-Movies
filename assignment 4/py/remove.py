b8432531185c81d763e55d384778d48ae6b7a0b0
Elias <eliaskalan@yahoo.gr>
2023-09-22 17:20:16 +0300

    push

--- assignment 4/py/remove.py
+++
@@ -1,8 +0,0 @@
-f=open("host.txt","r")
-while(True):
-    data=f.readline()
-    if(data):
-       if(data.__contains__("host_")):
-        print("Listings2",data.strip(),"TO",data.replace("host_","").strip(),";")
-    else:
-        break
